0485c197f54cacfb4627bae6d84184dadfea7b75
damien <contact.damiendiaz@gmail.com>
2024-09-12 15:10:32 +0200

    correction bug connection account onClick

--- src/features/auth/SignInButton.tsx
+++ src/features/auth/SignInButton.tsx
@@ -1,22 +1,20 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 import { LogIn } from "lucide-react";
-import { signInAction } from "./auth.action";
+import { signIn } from "next-auth/react";  // Import direct depuis next-auth
 
 export const SignInButton = () => {
     return (
-        <form>
         <Button
-        variant="secondary"
-        size="sm"
-        formAction={async () => {
-            await signInAction();
-        }}
-        className="bg-blue-500 text-white px-4 py-2 rounded"
+            variant="secondary"
+            size="sm"
+            onClick={async () => {
+                await signIn();  // Appelle signIn directement côté client
+            }}
+            className="bg-blue-500 text-white px-4 py-2 rounded"
         >
             <LogIn size={16} className="mr-2"></LogIn>Sign In
         </Button>
-        </form>
     );
 };
