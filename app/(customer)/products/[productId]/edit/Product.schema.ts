ae04dd3f1151880152ffc07fff3e7032bb99f7b8
damien <contact.damiendiaz@gmail.com>
2024-09-13 11:22:44 +0200

    form product begining

---
+++ app/(customer)/products/[productId]/edit/Product.schema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const ProductSchema = z.object({
+    name: z.string(),
+    noteText: z.string().optional(),
+    informationText: z.string().optional(),
+    reviewText: z.string().optional(),
+    thanksText: z.string().optional(),
+    backgroundColor: z.string(),
+});
+
+export type ProductType = z.infer<typeof ProductSchema>;
+
+export const GRADIENTS_CLASSES = [
+    "bg-gradient-to-r from-indigo-500 to-blue-500",
+    "bg-gradient-to-r from-fuchsia-600 to-purple-600",
+    "bg-gradient-to-r from-teal-200 to-teal-500"
+];
--- app/(customer)/products/[productId]/edit/ProductForm.tsx
+++ app/(customer)/products/[productId]/edit/ProductForm.tsx
@@ -1,17 +1,71 @@
 "use client"
 
-import { Card, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Form, FormField, FormLabel, FormControl, FormDescription, FormItem, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { GRADIENTS_CLASSES, ProductSchema, ProductType } from "./Product.schema";
+import { useZodForm } from "@/components/ui/form";
+import { Select, SelectTrigger, SelectValue, SelectContent } from "@radix-ui/react-select";
+import { SelectItem } from "@/components/ui/select";
+import { cn } from "@/lib/utils";
 
 export type ProductFormProps = {
-
-}
+    defaultValues?: ProductType;
+};
 
 export const ProductForm = (props: ProductFormProps) => {
+    const form = useZodForm({
+        schema: ProductSchema,
+        defaultValues: props.defaultValues,
+    });
+
+    const isCreate = !Boolean(props.defaultValues);
+
     return (
         <Card>
             <CardHeader>
-                <CardTitle>Create Product </CardTitle>
+                <CardTitle>
+                    {isCreate ? "Create product" : `Edit product ${props.defaultValues?.backgroundColor}`}
+                </CardTitle>
             </CardHeader>
+            <CardContent>
+                <Form
+                    form={form}
+                    onSubmit={async (values) => {
+                        console.log(values);
+                    }}
+                >
+                    <FormField
+                        control={form.control}
+                        name="backgroundColor"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Color</FormLabel>
+                                <FormControl>
+                                    <Select onValueChange={field.onChange} value={field.value}>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Select a color" />  {/* Utilisez un seul enfant ici */}
+                                        </SelectTrigger>
+                                        <SelectContent>
+                                            {GRADIENTS_CLASSES.map((gradient) => (
+                                                <SelectItem value={gradient} key={gradient}>
+                                                    <span className={cn(gradient, "w-full h-8 rounded-md")}></span>
+                                                </SelectItem>
+                                            ))}
+                                        </SelectContent>
+                                    </Select>
+                                    <Input placeholder="I Phone" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    The name of the product to review
+                                </FormDescription>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </Form>
+            </CardContent>
         </Card>
-    )
-}
+    );
+};
+                  
