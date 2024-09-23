0aefbb0263e0817041e05feeafb699babaa80c4b
damien <contact.damiendiaz@gmail.com>
2024-09-23 20:08:43 +0200

    modif logo height

--- src/features/layout/Header.tsx
+++ src/features/layout/Header.tsx
@@ -9,7 +9,7 @@ export const Header = async() => {
         <header className="w-full border-b border-border px-4">
         <Layout className="flex items-center flex-row gap-4">
             <div className="flex items-center gap-2">
-            <Image src="/icon.png" width={38} height={38} alt="dizzital-logo" />
+            <Image src="/icon.png" width={50} height={40} alt="dizzital-logo" />
             <span className="text-lg font-bold"> Dizzital</span>
             </div>
             <div className="flex items-center gap-4 ml-auto">
